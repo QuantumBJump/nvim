014c788fdabc2e88b79089b2996152e79b809516
Quinn Stevens <quinn@thoughtmachine.net>
2023-07-28 13:13:24 +0100

    fix: Ignore .git but not .gitignore

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -41,7 +41,7 @@ telescope.setup({
         find_files = {
             hidden = true,
             file_ignore_patterns = {
-                ".git",
+                "%.git$",
             },
         }
     },
