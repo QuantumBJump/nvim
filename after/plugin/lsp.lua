27463c4a216cbb3ff84b068c276b3444c338c496
Quinn Stevens <quinn@thoughtmachine.net>
2023-08-01 16:59:35 +0100

    fix: swap autocomplete prev & next

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,8 +13,8 @@ lsp.ensure_installed({
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-j>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-k>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
 })
