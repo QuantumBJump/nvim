5f3351bac3e1b17baeb11fa0e26380a659d57bd6
Quinn Stevens <quinn@thoughtmachine.net>
2024-05-10 14:01:45 +0100

    feat: harpoon marks per-branch

--- lua/quinns/packer.lua
+++ lua/quinns/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
             require('harpoon').setup(
                 {
                     tabline = true,
+                    mark_branch = true,
                 }
             )
         end
