7bcff2231c34d9c943c67da9a6fa2e0cd9ee4258
Quantum Jump <quantumjump451@gmail.com>
2024-11-08 15:20:15 +0000

    Go Transparent on Nixos

--- after/plugin/transparent.lua
+++ after/plugin/transparent.lua
@@ -8,6 +8,9 @@ require("transparent").setup({
 })
 
 -- only enable transparency if running on WSL - acrylic looks pretty
+if string.find(vim.loop.os_uname().version, "NixOS") == "NixOS" then
+    vim.cmd("TransparentEnable")
+end
 if vim.fn.has('wsl') == 1 then
     vim.cmd("TransparentEnable")
 else
