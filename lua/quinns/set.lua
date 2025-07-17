c144d6727780c98b3ebd662abae7480fee07d824
quinn <quinn@thoughtmachine.net>
2025-07-17 12:46:38 +0100

    Space indentation

--- lua/quinns/set.lua
+++ lua/quinns/set.lua
@@ -4,14 +4,14 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 -- 4-space tab is better than 8 but still disincentivises unnecessary nesting
-vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.opt.smarttab = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 -- I want undo files instead of backups
 vim.opt.swapfile = false
