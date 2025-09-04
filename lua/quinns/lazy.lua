1c60d03fa7bce7613f09d7a491b2b134cdd4f88b
quinn <quinn@thoughtmachine.net>
2025-09-04 15:02:32 +0100

    feat: upgrade to harpoon2

--- lua/quinns/lazy.lua
+++ lua/quinns/lazy.lua
@@ -100,15 +100,49 @@ require("lazy").setup({
     'nvim-treesitter/playground',
     {
         'theprimeagen/harpoon',
+        branch = 'harpoon2',
+        dependencies = { { 'nvim-lua/plenary.nvim' } },
         config = function()
-            require('harpoon').setup(
-                {
-                    global_settings = {
-                        tabline = true,
-                    },
-                })
+            local harpoon = require('harpoon')
+            harpoon:setup({
+            })
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'add file to Harpoon' })
+            vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+                { desc = 'open Harpoon quick menu' })
+
+            vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = 'open Harpoon file 1' })
+            vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = 'open Harpoon file 2' })
+            vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = 'open Harpoon file 3' })
+            vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = 'open Harpoon file 4' })
+            vim.keymap.set("n", "<leader>h5", function() harpoon:list():select(5) end, { desc = 'open Harpoon file 5' })
+            vim.keymap.set("n", "<leader>h6", function() harpoon:list():select(6) end, { desc = 'open Harpoon file 6' })
+            vim.keymap.set("n", "<leader>h7", function() harpoon:list():select(7) end, { desc = 'open Harpoon file 7' })
+            vim.keymap.set("n", "<leader>h8", function() harpoon:list():select(8) end, { desc = 'open Harpoon file 8' })
+            vim.keymap.set("n", "<leader>h9", function() harpoon:list():select(9) end, { desc = 'open Harpoon file 9' })
+            vim.keymap.set("n", "<leader>h0", function() harpoon:list():select(0) end, { desc = 'open Harpoon file 10' })
+
+            vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = 'next Harpoon file' })
+            vim.keymap.set("n", "<leader>hb", function() harpoon:list():prev() end, { desc = 'previous Harpoon file' })
         end
     },
+    {
+        "jasonpanosso/harpoon-tabline.nvim",
+        dependencies = { "ThePrimeagen/harpoon" },
+        config = function()
+            require('harpoon-tabline').setup()
+        end,
+    },
+    -- {
+    --     'theprimeagen/harpoon',
+    --     config = function()
+    --         require('harpoon').setup(
+    --             {
+    --                 global_settings = {
+    --                     tabline = true,
+    --                 },
+    --             })
+    --     end
+    -- },
     'mbbill/undotree',
     'tpope/vim-fugitive',
     { 'neovim/nvim-lspconfig' },
