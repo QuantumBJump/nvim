dcc26fd2d8fe67040c0fb46e68d15e2553330ffd
Quantum Jump <quantumjump451@gmail.com>
2026-07-22 11:06:12 +0100

    fix: update telescope and treesitter

--- lua/quinns/lazy.lua
+++ lua/quinns/lazy.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
         "nvim-telescope/telescope.nvim",
-        tag = '0.1.4',
+        tag = 'v0.2.1',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope-live-grep-args.nvim'
@@ -59,6 +59,7 @@ require("lazy").setup({
     'folke/tokyonight.nvim',
     {
         'nvim-treesitter/nvim-treesitter',
+        branch = "main",
         build = ":TSUpdate"
     },
     {
